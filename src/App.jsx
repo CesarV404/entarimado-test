bf05d3a4fd23483478fc3656f7131bb738bd0961
Cesar Vallejo <rawrcesar13@gmail.com>
2026-01-01 12:48:58 -0600

    beta finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,9 @@
 import { useState } from "react";
+import { MyDocument } from "./pdf-template";
+import { pdf } from "@react-pdf/renderer";
+
+import JsBarcode from "jsbarcode";
+
 import {
   BarcodeDetector,
   ZXING_WASM_VERSION,
@@ -27,6 +32,7 @@ export default function App() {
     hechoPor: "",
     superviso: "",
     piezas: "",
+    barcodeImg: null,
   });
 
   const [lista, setLista] = useState([]);
@@ -49,6 +55,7 @@ export default function App() {
       hechoPor: form.hechoPor,
       superviso: form.superviso,
       piezas: "",
+      barcodeImg: null,
     });
   };
 
@@ -60,12 +67,31 @@ export default function App() {
     setLista([]);
   };
 
+  function generarBarcodeBlob(valor) {
+    return new Promise((resolve, format) => {
+      const canvas = document.createElement("canvas");
+
+      JsBarcode(canvas, valor, {
+        format: "CODE128",
+        width: 2,
+        height: 60,
+        displayValue: false,
+      });
+
+      canvas.toBlob((blob) => {
+        resolve(blob);
+      }, "image/png");
+    });
+  }
+
   const handlePhoto = (e) => {
     const file = e.target.files[0];
+    console.log(file);
     if (!file) return;
 
-    barcodeDetector.detect(file).then((barcodes) => {
+    barcodeDetector.detect(file).then(async (barcodes) => {
       console.log(barcodes);
+
       if (barcodes.length === 0) {
         return setForm({
           ...form,
@@ -73,113 +99,135 @@ export default function App() {
         });
       }
 
+      const barcodeImgBlob = await generarBarcodeBlob(
+        String(barcodes[0].rawValue),
+        barcodes[0].format
+      );
+
+      const urlBarcode = URL.createObjectURL(barcodeImgBlob);
+
+      console.log(barcodeImgBlob);
+      console.log(urlBarcode);
+
       setForm({
         ...form,
         codigo: barcodes[0].rawValue,
+        barcodeImg: urlBarcode,
       });
     });
   };
 
+  const generarPDF = async (items) => {
+    const blob = await pdf(<MyDocument items={items} />).toBlob();
+
+    const url = URL.createObjectURL(blob);
+    const link = document.createElement("a");
+
+    link.href = url;
+    link.download = "documento.pdf";
+    link.click();
+
+    URL.revokeObjectURL(url);
+  };
+
   return (
-    <>
-      <div style={{ padding: 20, fontFamily: "Arial" }}>
-        <h2>Registro</h2>
+    <div style={{ padding: 20, fontFamily: "Arial" }}>
+      <h2>Registro</h2>
 
-        {/* FORMULARIO */}
-        <div style={{ marginBottom: 20 }}>
-          <input
-            name="descripcion"
-            placeholder="Descripción"
-            value={form.descripcion}
-            onChange={handleChange}
-          />
-          <br />
+      {/* FORMULARIO */}
+      <div style={{ marginBottom: 20 }}>
+        <input
+          name="descripcion"
+          placeholder="Descripción"
+          value={form.descripcion}
+          onChange={handleChange}
+        />
+        <br />
 
-          <input
-            name="codigo"
-            type="number"
-            placeholder="Código numérico"
-            value={form.codigo}
-            onChange={handleChange}
-          />
+        <input
+          name="codigo"
+          type="number"
+          placeholder="Código numérico"
+          value={form.codigo}
+          onChange={handleChange}
+        />
 
-          <br />
+        <br />
 
-          <input
-            type="file"
-            accept="image/*"
-            capture="environment"
-            onChange={handlePhoto}
-          />
-          <br />
+        <input
+          type="file"
+          accept="image/*"
+          capture="environment"
+          onChange={handlePhoto}
+        />
+        <br />
 
-          <input
-            name="hechoPor"
-            placeholder="Hecho por"
-            value={form.hechoPor}
-            onChange={handleChange}
-          />
-          <br />
+        <input
+          name="hechoPor"
+          placeholder="Hecho por"
+          value={form.hechoPor}
+          onChange={handleChange}
+        />
+        <br />
 
-          <input
-            name="superviso"
-            placeholder="Supervisó"
-            value={form.superviso}
-            onChange={handleChange}
-          />
-          <br />
+        <input
+          name="superviso"
+          placeholder="Supervisó"
+          value={form.superviso}
+          onChange={handleChange}
+        />
+        <br />
 
-          <input
-            name="piezas"
-            type="number"
-            placeholder="Piezas"
-            value={form.piezas}
-            onChange={handleChange}
-          />
-          <br />
-          <br />
+        <input
+          name="piezas"
+          type="number"
+          placeholder="Piezas"
+          value={form.piezas}
+          onChange={handleChange}
+        />
+        <br />
+        <br />
 
-          <button onClick={agregarItem}>Agregar</button>
-        </div>
-
-        {/* LISTA */}
-        <h3>Lista</h3>
-
-        {lista.length === 0 && <p>No hay registros</p>}
-
-        {lista.map((item) => (
-          <div
-            key={item.id}
-            style={{ border: "1px solid #ccc", padding: 10, marginBottom: 10 }}
-          >
-            <p>
-              <b>Descripción:</b> {item.descripcion}
-            </p>
-            <p>
-              <b>Código:</b> {item.codigo}
-            </p>
-            <p>
-              <b>Hecho por:</b> {item.hechoPor}
-            </p>
-            <p>
-              <b>Supervisó:</b> {item.superviso}
-            </p>
-            <p>
-              <b>Piezas:</b> {item.piezas}
-            </p>
-
-            <button onClick={() => eliminarItem(item.id)}>Eliminar</button>
-          </div>
-        ))}
-
-        {/* ACCIONES */}
-        {lista.length > 0 && (
-          <>
-            <button onClick={limpiarLista}>Borrar lista</button>{" "}
-            <button onClick={() => window.print()}>Imprimir</button>
-          </>
-        )}
+        <button onClick={agregarItem}>Agregar</button>
       </div>
-    </>
+
+      {/* LISTA */}
+      <h3>Lista</h3>
+
+      {lista.length === 0 && <p>No hay registros</p>}
+
+      {lista.map((item) => (
+        <div
+          key={item.id}
+          style={{ border: "1px solid #ccc", padding: 10, marginBottom: 10 }}
+        >
+          <p>
+            <b>Descripción:</b> {item.descripcion}
+          </p>
+          <p>
+            <b>Código:</b> {item.codigo}
+          </p>
+          <p>
+            <b>Hecho por:</b> {item.hechoPor}
+          </p>
+          <p>
+            <b>Supervisó:</b> {item.superviso}
+          </p>
+          <p>
+            <b>Piezas:</b> {item.piezas}
+          </p>
+
+          <button onClick={() => eliminarItem(item.id)}>Eliminar</button>
+        </div>
+      ))}
+
+      {/* ACCIONES */}
+      {lista.length > 0 && (
+        <>
+          <button onClick={limpiarLista}>Borrar lista</button>{" "}
+          <button onClick={async () => generarPDF(lista)}>Imprimir</button>
+        </>
+      )}
+    </div>
   );
 }
